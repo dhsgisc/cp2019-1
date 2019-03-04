82fa0aad394beb6bf9eac62af166f590e2dcf748
tianchi02 <47800377+tianchi02@users.noreply.github.com>
2019-03-04 19:04:33 +0800

    Create q12_find_factors.py

---
+++ q12_find_factors.py
@@ -0,0 +1,14 @@
+num = int(input("Enter Number: "))
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    print(factors)
+prime_factors(num)
