755e18f4259bbdbb64ad2b062f9aa332649cb0ee
tianchi02 <47800377+tianchi02@users.noreply.github.com>
2019-03-04 18:55:57 +0800

    Update q02_triangle.py

--- q02_triangle.py
+++ q02_triangle.py
@@ -1,1 +1,13 @@
+side_1 = int(input("Enter side 1: "))
+side_2 = int(input("Enter side 2: "))
+side_3 = int(input("Enter side 3: "))
+if side_1+side_2 <= side_3:
+  print("Invaild Triangle!")
+elif side_2+side_3 <= side_1:
+  print("Invaild Triangle!")
+elif side_1+side_3 <= side_2:
+  print("Invaild Triangle!")
+else:
+  peri = side_1+side_2+side_3
+  print("Perimeter is: " + str(peri))
 
